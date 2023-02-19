aa446c5f7547d588f23f2a40178452b3e83e283b
Carlos Jessiel <c.jessiel.17@gmail.com>
2023-02-18 22:07:11 -0300

    Raiz do projeto

---
+++ src/test/java/br/com/service/email/servicesendemail/ServiceSendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.service.email.servicesendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
